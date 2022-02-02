fc25e8adc6bdc42c246c0d1919ae4c94a325b6d9
Rajarshi Basak <rajarshi.basak@colorado.edu>
2022-02-02 00:09:29 -0500

    Update 001_two_sum.py
    One pass

--- Top Int Ques/001_two_sum.py
+++ Top Int Ques/001_two_sum.py
@@ -13,3 +13,19 @@ class Solution(object):
                     result = [i, j]
                     
         return result
+
+ class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        result = []
+        
+        for i in range(0, len(nums)):
+            j = target - nums[i]
+            if j in nums and i != nums.index(j):
+                result = [i, nums.index(j)]
+                   
+        return result
