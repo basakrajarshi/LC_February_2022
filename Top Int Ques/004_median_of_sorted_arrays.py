0d3582341ee675b545b85c37383ef5810761b7d0
Rajarshi Basak <rajarshi.basak@colorado.edu>
2022-02-06 01:05:53 -0500

    Create 004_median_of_sorted_arrays.py

---
+++ Top Int Ques/004_median_of_sorted_arrays.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        nums = sorted(nums1 + nums2)
+        median = 0.0
+        middle_val = len(nums)//2
+
+        
+        if len(nums)%2 == 0:
+            median = (nums[middle_val-1] + nums[middle_val])/2.0
+        else:
+            median = nums[middle_val]
+            
+        return median
