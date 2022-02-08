6dbbcf5af27d509ef28ebe32da873fc7d3b82770
Rajarshi Basak <rajarshi.basak@colorado.edu>
2022-02-07 23:06:38 -0500

    Update 005_longest_palindromic_substring.py

--- Top Int Ques/005_longest_palindromic_substring.py
+++ Top Int Ques/005_longest_palindromic_substring.py
@@ -26,3 +26,35 @@ class Solution(object):
                 end = i + (maxl)//2
                 
         return s[start:end+1]
+
+    class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = ''
+        reslen = 0
+        
+        for i in range(len(s)):
+            # odd length
+            l = i
+            r = i
+            while (l >= 0 and r < len(s) and s[r] == s[l]):
+                if (r - l + 1) > reslen:
+                    res = s[l:r+1]
+                    reslen = r - l + 1
+                l -= 1
+                r += 1
+            
+            # even length
+            l =  i
+            r = i+1
+            while (l >= 0 and r < len(s) and s[r] == s[l]):
+                if (r - l + 1) > reslen:
+                    res = s[l:r+1]
+                    reslen = r - l + 1
+                l -= 1
+                r += 1
+                
+        return res
